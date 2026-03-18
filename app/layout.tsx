d7d8a9bd1f1353f91e30152f09cc81f473a35b05
Michael Li <michael.li@atomicobject.com>
2026-03-18 13:20:25 -0400

    add credits

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        {children}
+        <main className="flex-1">
+          {children}
+        </main>
+        <footer className="py-4 text-center text-sm text-gray-500 border-t border-gray-200">
+          Created by{' '}
+          <a
+            href="https://mahculee.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-indigo-600 hover:text-indigo-700 font-medium"
+          >
+            Mahculee
+          </a>
+        </footer>
         <Toaster
           position="top-center"
           toastOptions={{
